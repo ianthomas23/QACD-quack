Correct scaling of element maps and mode handlers

src/gui/matplotlib_widget.py
@@ -8,6 +8,7 @@ from PyQt5 import QtCore, QtWidgets
 
 from .enums import ArrayType, ModeType, PlotType
 from .mode_handler import *
+from .scale_bar import ScaleBar
 
 
 class MatplotlibWidget(QtWidgets.QWidget):
@@ -22,11 +23,17 @@ class MatplotlibWidget(QtWidgets.QWidget):
         self._display_options = None
         self._map_axes = None
 
+        # Variables set by call to update()
+        self._plot_type = PlotType.INVALID
+        self._array_type = ArrayType.INVALID
+        self._array = None
+        self._array_stats = None
+        self._title = None
+
         self._image = None       # Image used for element map.
         self._bar = None         # Bar used for histogram.
         self._bar_norm_x = None  # Normalised x-positions of centres of bars
                                  #   in range 0 (= min) to 1.0 (= max value).
-        self._array_type = ArrayType.INVALID
         self._cmap_int_max = None  # One beyond end, as in numpy slicing.
 
         # Initialised in initialise().
@@ -66,15 +73,139 @@ class MatplotlibWidget(QtWidgets.QWidget):
         self._canvas.draw()
         #self._canvas.draw_idle()
 
+    def _update_draw(self):
+        # Draw using cached variables.
+        # Derived quantities.
+        show_colourbar = True
+        cmap_int_max = None
+        if self._array_type == ArrayType.CLUSTER:
+            cmap_int_max = self._array_stats['max'] + 1
+        elif self._array_type in (ArrayType.PHASE, ArrayType.REGION):
+            show_colourbar = False
+            cmap_int_max = 2
+
+        self._cmap_int_max = cmap_int_max
+
+        figure = self._canvas.figure
+        figure.clear()
+
+        options = self._display_options
+        map_axes = None
+        histogram_axes = None
+        if self._plot_type == PlotType.MAP:
+            map_axes = figure.subplots()
+        elif self._plot_type == PlotType.HISTOGRAM:
+            histogram_axes = figure.subplots()
+        elif self._plot_type == PlotType.BOTH:
+            map_axes, histogram_axes = figure.subplots( \
+                nrows=2, gridspec_kw={'height_ratios': (3,1)})
+        else:
+            raise RuntimeError('Invalid plot type')
+
+        cmap = self.create_colourmap()
+
+        if cmap_int_max is None:
+            show_stats = True
+            norm = Normalize(self._array_stats['min'], self._array_stats['max'])
+            cmap_ticks = None
+        else:
+            show_stats = False
+            norm = Normalize(-0.5, cmap_int_max-0.5)
+            cmap_ticks = np.arange(0, cmap_int_max)
+            if cmap_int_max >= 15:
+                cmap_ticks = cmap_ticks[::2]
+
+        if map_axes is None:
+            self._image = None
+        else:
+            ny, nx = self._array.shape
+            extent = np.array([0.0, nx, ny, 0.0])
+            if options.use_scale:
+                units = options.units
+                extent *= options.scale
+                if extent.max() >= 1000.0:
+                    units = options.get_next_larger_units(units)
+                    extent /= 1000.0
+            else:
+                units = 'pixels'
+            self._image = map_axes.imshow(self._array, cmap=cmap, norm=norm,
+                                          extent=extent)
+            map_axes.set_xlabel(units)
+            map_axes.set_ylabel(units)
+
+            if show_colourbar:
+                colourbar = figure.colorbar(self._image, ax=map_axes,
+                                            ticks=cmap_ticks)
+            if self._title is not None:
+                map_axes.set_title(self._title + ' map')
+            if self._map_xlim is not None:
+                scale = self._display_options.scale
+                map_axes.set_xlim(self._map_xlim*scale)
+                map_axes.set_ylim(self._map_ylim*scale)
+
+            # Trying out scale bar.
+            #sb = ScaleBar(ax=map_axes, size=30, label='1 mm', loc=3,
+            #              frameon=False, pad=0.6, sep=4)
+            #map_axes.add_artist(sb)
+
+        if histogram_axes is None:
+            self._bar = None
+            self._bar_norm_x = None
+        else:
+            if cmap_int_max is None:
+                bins = 100
+            else:
+                bins = np.arange(0, cmap_int_max+1)-0.5
+            hist, bin_edges = np.histogram(np.ma.compressed(self._array),
+                                           bins=bins)
+            width = bin_edges[1] - bin_edges[0]
+            bin_centres = bin_edges[:-1] + 0.5*width
+            self._bar_norm_x = norm(bin_centres)
+            colours = cmap(self._bar_norm_x)
+            self._bar = histogram_axes.bar(bin_centres, hist, width,
+                                           color=colours)
+            if cmap_ticks is not None:
+                histogram_axes.set_xticks(cmap_ticks)
+
+            if show_stats:
+                mean = self._array_stats.get('mean')
+                median = self._array_stats.get('median')
+                std = self._array_stats.get('std')
+                if mean is not None:
+                    histogram_axes.axvline(mean, c='k', ls='-', label='mean')
+                    if std is not None:
+                        histogram_axes.axvline(mean-std, c='k', ls='-.',
+                                               label='mean \u00b1 std')
+                        histogram_axes.axvline(mean+std, c='k', ls='-.')
+                if median is not None:
+                    histogram_axes.axvline(median, c='k', ls='--',
+                                           label='median')
+                histogram_axes.legend()
+
+            if map_axes is None and self._title is not None:
+                histogram_axes.set_title(self._title + ' histogram')
+
+        self._map_axes = map_axes
+        self._adjust_layout()
+
+        if self._mode_handler:
+            self._mode_handler.move_to_new_axes()
+
+        self._redraw()
+
     def clear(self):
         # Clear current plots.
         self._zoom_rectangle = None
         self._canvas.figure.clear()
         self._map_axes = None
+        self._plot_type = PlotType.INVALID
+        self._array_type = ArrayType.INVALID
+        self._array = None
+        self._array_stats = None
+        self._title = None
         self._image = None
         self._bar = None
         self._bar_norm_x = None
-        self._array_type = ArrayType.INVALID
         self._cmap_int_max = None  # One beyond end, as in numpy slicing.
 
         self._redraw()
@@ -143,18 +274,26 @@ class MatplotlibWidget(QtWidgets.QWidget):
 
     def set_display_options(self, display_options):
         if self._display_options is not None:
-            self._display_options.remove_listener(self)
+            self._display_options.unregister_listener(self)
         self._display_options = display_options
         if self._display_options is not None:
-            self._display_options.add_listener(self)
+            self._display_options.register_listener(self)
+
+        if self._mode_handler is not None:
+            self._mode_handler.set_display_options(display_options)
 
     def set_map_zoom(self, xs, ys):
         if self._map_axes is not None:
+            scale = self._display_options.scale
+
+            xs = np.asarray(xs)
+            ys = np.asarray(ys)
+
             self._map_xlim = xs
             self._map_ylim = ys
 
-            self._map_axes.set_xlim(xs)
-            self._map_axes.set_ylim(ys)
+            self._map_axes.set_xlim(xs*scale)
+            self._map_axes.set_ylim(ys*scale)
             self._redraw()
 
     def set_mode_type(self, mode_type, listener=None):
@@ -164,116 +303,27 @@ class MatplotlibWidget(QtWidgets.QWidget):
             if self._mode_handler:
                 self._mode_handler.clear()
 
+            options = self._display_options
+
             if mode_type == ModeType.ZOOM:
-                self._mode_handler = ZoomHandler(self)
+                self._mode_handler = ZoomHandler(self, options)
             elif mode_type == ModeType.REGION_RECTANGLE:
-                self._mode_handler = RectangleRegionHandler(self, listener)
+                self._mode_handler = RectangleRegionHandler(self, options, listener)
             elif mode_type == ModeType.REGION_ELLIPSE:
-                self._mode_handler = EllipseRegionHandler(self, listener)
+                self._mode_handler = EllipseRegionHandler(self, options, listener)
             elif mode_type == ModeType.REGION_POLYGON:
-                self._mode_handler = PolygonRegionHandler(self, listener)
+                self._mode_handler = PolygonRegionHandler(self, options, listener)
             else:
                 self._mode_handler = None
 
     def update(self, plot_type, array_type, array, array_stats, title):
-        # Derived quantities.
-        show_colourbar = True
-        cmap_int_max = None
-        if array_type == ArrayType.CLUSTER:
-            cmap_int_max = array_stats['max'] + 1
-        elif array_type in (ArrayType.PHASE, ArrayType.REGION):
-            show_colourbar = False
-            cmap_int_max = 2
-
+        self._plot_type = plot_type
         self._array_type = array_type
-        self._cmap_int_max = cmap_int_max
+        self._array = array
+        self._array_stats = array_stats
+        self._title = title
 
-        figure = self._canvas.figure
-        figure.clear()
-
-        map_axes = None
-        histogram_axes = None
-        if plot_type == PlotType.MAP:
-            map_axes = figure.subplots()
-        elif plot_type == PlotType.HISTOGRAM:
-            histogram_axes = figure.subplots()
-        elif plot_type == PlotType.BOTH:
-            map_axes, histogram_axes = figure.subplots( \
-                nrows=2, gridspec_kw={'height_ratios': (3,1)})
-        else:
-            raise RuntimeError('Invalid plot type')
-
-        cmap = self.create_colourmap()
-
-        if cmap_int_max is None:
-            show_stats = True
-            norm = Normalize(array_stats['min'], array_stats['max'])
-            cmap_ticks = None
-        else:
-            show_stats = False
-            norm = Normalize(-0.5, cmap_int_max-0.5)
-            cmap_ticks = np.arange(0, cmap_int_max)
-            if cmap_int_max >= 15:
-                cmap_ticks = cmap_ticks[::2]
-
-        if map_axes is None:
-            self._image = None
-        else:
-            self._image = map_axes.imshow(array, cmap=cmap, norm=norm)
-            if show_colourbar:
-                colourbar = figure.colorbar(self._image, ax=map_axes,
-                                            ticks=cmap_ticks)
-            if title is not None:
-                map_axes.set_title(title + ' map')
-            if self._map_xlim is not None:
-                map_axes.set_xlim(self._map_xlim)
-                map_axes.set_ylim(self._map_ylim)
-            #map_axes.set_xlabel('pixels')
-            #map_axes.set_ylabel('pixels')
-
-        if histogram_axes is None:
-            self._bar = None
-            self._bar_norm_x = None
-        else:
-            if cmap_int_max is None:
-                bins = 100
-            else:
-                bins = np.arange(0, cmap_int_max+1)-0.5
-            hist, bin_edges = np.histogram(np.ma.compressed(array), bins=bins)
-            width = bin_edges[1] - bin_edges[0]
-            bin_centres = bin_edges[:-1] + 0.5*width
-            self._bar_norm_x = norm(bin_centres)
-            colours = cmap(self._bar_norm_x)
-            self._bar = histogram_axes.bar(bin_centres, hist, width,
-                                           color=colours)
-            if cmap_ticks is not None:
-                histogram_axes.set_xticks(cmap_ticks)
-
-            if show_stats:
-                mean = array_stats.get('mean')
-                median = array_stats.get('median')
-                std = array_stats.get('std')
-                if mean is not None:
-                    histogram_axes.axvline(mean, c='k', ls='-', label='mean')
-                    if std is not None:
-                        histogram_axes.axvline(mean-std, c='k', ls='-.',
-                                               label='mean \u00b1 std')
-                        histogram_axes.axvline(mean+std, c='k', ls='-.')
-                if median is not None:
-                    histogram_axes.axvline(median, c='k', ls='--',
-                                           label='median')
-                histogram_axes.legend()
-
-            if map_axes is None and title is not None:
-                histogram_axes.set_title(title + ' histogram')
-
-        self._map_axes = map_axes
-        self._adjust_layout()
-
-        if self._mode_handler:
-            self._mode_handler.move_to_new_axes()
-
-        self._redraw()
+        self._update_draw()
 
     def update_colourmap_name(self):
         colourmap_name = self._display_options.colourmap_name
@@ -291,3 +341,6 @@ class MatplotlibWidget(QtWidgets.QWidget):
                 item.set_color(colours[index])
 
         self._redraw()
+
+    def update_scale(self):
+        self._update_draw()

src/gui/mode_handler.py
@@ -8,8 +8,9 @@ from PyQt5 import QtCore, QtWidgets
 # Classes to handle different interactive modes in matplotlib_widget.
 
 class ModeHandler:
-    def __init__(self, matplotlib_widget):
+    def __init__(self, matplotlib_widget, display_options):
         self.matplotlib_widget = matplotlib_widget
+        self.display_options = display_options
 
     def clear(self):
         pass
@@ -32,11 +33,14 @@ class ModeHandler:
     def on_mouse_move(self, event):
         pass
 
+    def set_display_options(self, display_options):
+        self.display_options = display_options
+
 
 # Abstract base class for all region handlers.
 class RegionHandler(ModeHandler):
-    def __init__(self, matplotlib_widget, listener):
-        super().__init__(matplotlib_widget)
+    def __init__(self, matplotlib_widget, display_options, listener):
+        super().__init__(matplotlib_widget, display_options)
         self._listener = listener
         self._line_colour = 'k'
         self._shadow_colour = 'w'
@@ -54,8 +58,11 @@ class RegionHandler(ModeHandler):
             masked = np.ma.masked_equal(self._region, True)
 
             cmap = self.matplotlib_widget._create_white_colourmap()
+
+            ny, nx = masked.shape
+            extent = np.array([0.0, nx, ny, 0.0])*self.display_options.scale
             return self.matplotlib_widget._map_axes.imshow( \
-                masked, alpha=self._region_alpha, cmap=cmap)
+                masked, alpha=self._region_alpha, cmap=cmap, extent=extent)
         else:
             return None
 
@@ -127,8 +134,8 @@ class RegionHandler(ModeHandler):
 # Abstract base class for region handlers that are controlled by a single drag
 # of the mouse.
 class MouseDragRegionHandler(RegionHandler):
-    def __init__(self, matplotlib_widget, listener):
-        super().__init__(matplotlib_widget, listener)
+    def __init__(self, matplotlib_widget, display_options, listener):
+        super().__init__(matplotlib_widget, display_options, listener)
 
     def on_mouse_down(self, event):
         if (not self._editing and
@@ -142,6 +149,7 @@ class MouseDragRegionHandler(RegionHandler):
             self._editing = True
             self._points = np.asarray([[event.xdata, event.ydata],
                                        [event.xdata, event.ydata]])
+            self._points /= self.display_options.scale
             self._artists = self._create_artists()
             self.matplotlib_widget._redraw()
 
@@ -150,6 +158,7 @@ class MouseDragRegionHandler(RegionHandler):
             event.inaxes == self.matplotlib_widget._map_axes):
 
             self._points[1] = (event.xdata, event.ydata)
+            self._points[1] /= self.display_options.scale
             self._move_artists()
             self.matplotlib_widget._redraw()
 
@@ -161,14 +170,13 @@ class MouseDragRegionHandler(RegionHandler):
 
 
 class EllipseRegionHandler(MouseDragRegionHandler):
-    def __init__(self, matplotlib_widget, listener):
-        super().__init__(matplotlib_widget, listener)
+    def __init__(self, matplotlib_widget, display_options, listener):
+        super().__init__(matplotlib_widget, display_options, listener)
 
     def _calculate_region(self):
         project = self.matplotlib_widget._owning_window._project
         if project is not None:
-            centre = self._artists[0].center
-            size = (self._artists[0].width, self._artists[0].height)
+            centre, size = self._get_centre_and_size(False)
             return project.calculate_region_ellipse(centre, size)
         else:
             return None
@@ -177,22 +185,26 @@ class EllipseRegionHandler(MouseDragRegionHandler):
         if len(self._points) < 2:
             return []
 
-        centre, size = self._get_centre_and_size()
+        scale = self.display_options.scale
+        centre, size = self._get_centre_and_size(True)
         ellipse = Ellipse(centre, width=size[0], height=size[1],
                           fc='none', ec=self._line_colour)
         ellipse = self.matplotlib_widget._map_axes.add_patch(ellipse)
         ellipse.set_path_effects(self._get_path_effects())
         return [ellipse]
 
-    def _get_centre_and_size(self):
+    def _get_centre_and_size(self, scaled):
         start = self._points[0]
         end = self._points[1]
         centre = (start + end)*0.5
         size = end - start
+        if scaled:
+            centre *= self.display_options.scale
+            size *= self.display_options.scale
         return centre, size
 
     def _move_artists(self):
-        centre, size = self._get_centre_and_size()
+        centre, size = self._get_centre_and_size(True)
         self._artists[0].center = centre
         self._artists[0].width  = size[0]
         self._artists[0].height = size[1]
@@ -203,15 +215,14 @@ class EllipseRegionHandler(MouseDragRegionHandler):
 
 
 class RectangleRegionHandler(MouseDragRegionHandler):
-    def __init__(self, matplotlib_widget, listener):
-        super().__init__(matplotlib_widget, listener)
+    def __init__(self, matplotlib_widget, display_options, listener):
+        super().__init__(matplotlib_widget, display_options, listener)
 
     def _calculate_region(self):
         project = self.matplotlib_widget._owning_window._project
         if project is not None:
-            corner0 = self._artists[0].get_xy()
-            corner1 = (corner0[0] + self._artists[0].get_width(),
-                       corner0[1] + self._artists[0].get_height())
+            corner0 = self._points[0]
+            corner1 = self._points[1]
             return project.calculate_region_rectangle(corner0, corner1)
         else:
             return None
@@ -220,8 +231,8 @@ class RectangleRegionHandler(MouseDragRegionHandler):
         if len(self._points) < 2:
             return []
 
-        start = self._points[0]
-        size = self._points[1] - start
+        start = self._points[0]*self.display_options.scale
+        size  = self._points[1]*self.display_options.scale - start
         rectangle = Rectangle(start, width=size[0], height=size[1],
                               fc='none', ec=self._line_colour)
         rectangle = self.matplotlib_widget._map_axes.add_patch(rectangle)
@@ -229,8 +240,8 @@ class RectangleRegionHandler(MouseDragRegionHandler):
         return [rectangle]
 
     def _move_artists(self):
-        start = self._points[0]
-        end = self._points[1]
+        start = self._points[0]*self.display_options.scale
+        end   = self._points[1]*self.display_options.scale
         self._artists[0].set_width( end[0] - start[0])
         self._artists[0].set_height(end[1] - start[1])
 
@@ -240,8 +251,8 @@ class RectangleRegionHandler(MouseDragRegionHandler):
 
 class PolygonRegionHandler(RegionHandler):
     # 3 artists: lines, markers and highlight marker.
-    def __init__(self, matplotlib_widget, listener):
-        super().__init__(matplotlib_widget, listener)
+    def __init__(self, matplotlib_widget, display_options, listener):
+        super().__init__(matplotlib_widget, display_options, listener)
 
     def _add_point(self, point):
         # Add point to end of self._points, but only if it is not the same as
@@ -252,8 +263,11 @@ class PolygonRegionHandler(RegionHandler):
             if not self._artists:
                 self._artists = self._create_artists()
             else:
-                self._artists[0].set_data(self._points[:, 0], self._points[:, 1])
-                self._artists[1].set_data(self._points[:, 0], self._points[:, 1])
+                scale = self.display_options.scale
+                self._artists[0].set_data(self._points[:, 0]*scale,
+                                          self._points[:, 1]*scale)
+                self._artists[1].set_data(self._points[:, 0]*scale,
+                                          self._points[:, 1]*scale)
             self.matplotlib_widget._redraw()
 
     def _calculate_region(self):
@@ -266,8 +280,8 @@ class PolygonRegionHandler(RegionHandler):
     def _close_polygon(self):
         if self._artists:
             self._points = np.vstack((self._points, self._points[0]))
-            x = self._points[:, 0]
-            y = self._points[:, 1]
+            x = self._points[:, 0]*self.display_options.scale
+            y = self._points[:, 1]*self.display_options.scale
             self._artists[0].set_data(x, y)
             self._artists[1].set_data(x, y)
 
@@ -275,10 +289,11 @@ class PolygonRegionHandler(RegionHandler):
 
     def _create_artists(self):
         colour = self._line_colour
+        scale = self.display_options.scale
         lines = self.matplotlib_widget._map_axes.plot( \
-            self._points[:, 0], self._points[:, 1], '-', c=colour)[0]
+            self._points[:, 0]*scale, self._points[:, 1]*scale, '-', c=colour)[0]
         markers = self.matplotlib_widget._map_axes.plot(\
-            self._points[:, 0], self._points[:, 1], 'o', c=colour)[0]
+            self._points[:, 0]*scale, self._points[:, 1]*scale, 'o', c=colour)[0]
 
         path_effects = self._get_path_effects()
         lines.set_path_effects(path_effects)
@@ -299,7 +314,9 @@ class PolygonRegionHandler(RegionHandler):
             if event.dblclick or (self._artists and self._artists[2]):
                 self._close_polygon()
             else:
-                self._add_point((event.xdata, event.ydata))
+                scale = self.display_options.scale
+                point = np.asarray((event.xdata, event.ydata)) / scale
+                self._add_point(point)
 
     def on_mouse_move(self, event):
         if (self._artists and self._artists[0] is not None and
@@ -308,8 +325,10 @@ class PolygonRegionHandler(RegionHandler):
             inside, matches = self._artists[1].contains(event)
             if inside:
                 if self._artists[2] is None and 0 in matches['ind']:
+                    scale = self.display_options.scale
                     self._artists[2] = self.matplotlib_widget._map_axes.plot(\
-                        self._points[0, 0], self._points[0, 1], 'o', c='yellow')[0]
+                        self._points[0, 0]*scale, self._points[0, 1]*scale,
+                        'o', c='yellow')[0]
                     self.matplotlib_widget._redraw()
             elif self._artists[2]:
                 self._artists[2].remove()
@@ -318,8 +337,8 @@ class PolygonRegionHandler(RegionHandler):
 
 
 class ZoomHandler(ModeHandler):
-    def __init__(self, matplotlib_widget):
-        super().__init__(matplotlib_widget)
+    def __init__(self, matplotlib_widget, display_options):
+        super().__init__(matplotlib_widget, display_options)
         self._zoom_rectangle = None  # Only set when zooming.
 
     def clear(self):
@@ -377,10 +396,11 @@ class ZoomHandler(ModeHandler):
                 zoom_xs = sorted([x, x+width])
                 zoom_ys = sorted([y, y+height], reverse=True)
 
-                from_ = (self.matplotlib_widget._map_axes.get_xlim(),
-                         self.matplotlib_widget._map_axes.get_ylim())
+                scale = self.display_options.scale
+                from_ = np.asarray((self.matplotlib_widget._map_axes.get_xlim(),
+                                    self.matplotlib_widget._map_axes.get_ylim())) / scale
+                to = np.asarray((zoom_xs, zoom_ys)) / scale
 
-                self.matplotlib_widget._owning_window.zoom_append( \
-                    from_=from_, to=(zoom_xs, zoom_ys))
+                self.matplotlib_widget._owning_window.zoom_append(from_=from_, to=to)
 
             self.clear()

src/model/qacd_project.py
@@ -127,6 +127,11 @@ class QACDProject:
         mask = node.read()
         return mask
 
+    def _get_pixel_grid(self):
+        shape = self.shape
+        x, y = np.meshgrid(np.arange(shape[1]) + 0.5, np.arange(shape[0]) + 0.5)
+        return x, y
+
     @contextmanager
     def _h5file(self):
         # All writing to h5 file is done in a 'with self._h5file() as f' block.
@@ -159,22 +164,19 @@ class QACDProject:
     def calculate_region_ellipse(self, centre, size):
         # Calculate and return boolean array corresponding to ellipse region.
         # Does not store the region.  centre and size are 2-tuples.
-        shape = self.shape
-        x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
+        x, y = self._get_pixel_grid()
         return utils.calculate_region_ellipse(x, y, centre, size)
 
     def calculate_region_polygon(self, points):
         # Calculate and return boolean array corresponding to polygon region.
         # Does not store the region.  points is a numpy array of shape (n, 2).
-        shape = self.shape
-        x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
+        x, y = self._get_pixel_grid()
         return utils.calculate_region_polygon(x, y, points)
 
     def calculate_region_rectangle(self, corner0, corner1):
         # Calculate and return boolean array corresponding to rectangle region.
         # Does not store the region.  corner0 and corner1 are 2-tuples.
-        shape = self.shape
-        x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
+        x, y = self._get_pixel_grid()
         return utils.calculate_region_rectangle(x, y, corner0, corner1)
 
     def create_h_factor(self, progress_callback=None):